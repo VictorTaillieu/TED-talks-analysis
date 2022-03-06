84df9015dbd4eaa7c10f3be304b9413e7969f93d
AirRunner <luca.vaio@efrei.net>
2022-03-05 19:44:49 -0500

    Update data script No need to load kaggle api key, just store it into ~/.kaggle/kaggle.json

--- data/get_data.py
+++ data/get_data.py
@@ -1,16 +1,8 @@
-import os
-from json import load as json_load
-
-with open("kaggle.json", 'r') as kaggle_credentials:
-    kaggle_cred = json_load(kaggle_credentials)
-
-os.environ["KAGGLE_USERNAME"] = kaggle_cred["username"]
-os.environ["KAGGLE_KEY"] = kaggle_cred["key"]
-
+from os import rename
 from kaggle.api.kaggle_api_extended import KaggleApi
 
 api = KaggleApi()
 api.authenticate()
 api.dataset_download_files("miguelcorraljr/ted-ultimate-dataset", quiet=False, unzip=True)
 
-os.rename("2020-05-01", "raw")
+rename("2020-05-01", "raw")

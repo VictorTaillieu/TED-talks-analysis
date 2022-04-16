7736fb1473ad1436fea3318d489660e2ef918bc8
MaximeLafont <maxime.lafont.trevisan@gmail.com>
2022-04-16 18:48:49 -0400

    All in distance matrix + new distance

--- model.py
+++ model.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import pairwise_distances
 from sklearn.preprocessing import MinMaxScaler
+from ast import literal_eval
+from scipy.spatial import distance
 
 
 class TEDflix:
@@ -15,12 +17,23 @@ class TEDflix:
                 1,  # description
                 1,  # about_speaker
                 1,  # general_infos
-                1,  # topics
+                1,  # related dist
                 1,  # sentiment
-                1   # dates
+                1,  # dates
+                1   # topics dist
             ]).reshape(-1, 1)
         else:
             self.weights = weights
+
+
+    @staticmethod
+    def boolean_df(item_lists, unique_items):
+        bool_dict = {}
+        
+        for item in unique_items:
+            bool_dict[item] = item_lists.apply(lambda x: item in x)
+        
+        return pd.DataFrame(bool_dict)
 
 
     @staticmethod
@@ -54,8 +67,18 @@ class TEDflix:
         np.fill_diagonal(self.gen_dist, np.max(self.gen_dist))  # Which value to put between a talk and itself?
 
         # Topic distance
-        self.topic_dist = np.load("data/embeddings/related_distance.npy")
-        np.fill_diagonal(self.topic_dist, np.max(self.topic_dist))
+        self.related_dist = np.load("data/embeddings/related_distance.npy")
+        np.fill_diagonal(self.related_dist, np.max(self.related_dist))
+
+        top = self.df.topics.apply(lambda x: literal_eval(x))
+        one_hot_topics = TEDflix.boolean_df(top, top.explode().unique()).astype(int)
+
+        self.topics_dist = distance.squareform(distance.pdist(one_hot_topics,"jaccard"))
+        np.fill_diagonal(self.topics_dist, np.max(self.topics_dist))
+
+        self.sent_dist = pairwise_distances(np.array(self.df.sentiment).reshape(-1,1), metric="l1")
+
+        self.date_dist = pairwise_distances(np.array(self.df.recorded_date).reshape(-1,1), metric="l1")
 
         self.trained = True
     
@@ -69,9 +92,10 @@ class TEDflix:
         desc_cum_dist = TEDflix.cum_dist(self.desc_dist, history)
         speak_cum_dist = TEDflix.cum_dist(self.speak_dist, history)
         gen_cum_dist = TEDflix.cum_dist(self.gen_dist, history)
-        topic_cum_dist = TEDflix.cum_dist(self.topic_dist, history)
-        sent_cum_dist = np.mean([abs(self.df.sentiment[i] - self.df.sentiment) for i in history], axis=0)
-        date_cum_dist = np.mean([abs(self.df.recorded_date[i] - self.df.recorded_date) for i in history], axis=0)
+        related_cum_dist = TEDflix.cum_dist(self.related_dist, history)
+        topics_cum_dist = TEDflix.cum_dist(self.topics_dist, history)
+        sent_cum_dist = TEDflix.cum_dist(self.sent_dist, history)
+        date_cum_dist = TEDflix.cum_dist(self.date_dist, history)
 
         # Combine distances
         combined_dist_matrix = np.array([
@@ -79,9 +103,10 @@ class TEDflix:
                 desc_cum_dist,
                 speak_cum_dist,
                 gen_cum_dist,
-                topic_cum_dist,
+                related_cum_dist,
                 sent_cum_dist,
-                date_cum_dist
+                date_cum_dist,
+                topics_cum_dist
             ]
         ])
 
